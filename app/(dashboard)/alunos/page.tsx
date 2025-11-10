56657e32b0e2f4d0b785f87115e9cec3d7580c09
jvras <jonathas.silva@viitra.com>
2025-11-10 16:41:32 -0300

    feat: criar AlunosLayout e refatorar AlunosPage para melhor estruturação

--- app/(dashboard)/alunos/page.tsx
+++ app/(dashboard)/alunos/page.tsx
@@ -1,10 +1,8 @@
-import { AlunosProvider } from "@/modules/dashboard/alunos/providers/alunos-provider"
+
 import { AlunosDashboard } from "@/modules/dashboard/alunos/components/aluno-components"
 
 export default function AlunosPage() {
   return (
-    <AlunosProvider>
       <AlunosDashboard />
-    </AlunosProvider>
   )
 }
